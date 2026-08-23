1a4ac909b8c3d098c72b3518ace5f80896d53207
pancursolucoes <pancursolucoes@gmail.com>
2026-08-23 19:08:26 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
+// import { TooltipProvider } from "@/components/ui/tooltip";
 // import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
