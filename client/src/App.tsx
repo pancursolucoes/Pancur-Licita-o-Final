b552a8a678a11f87af8294a8efa9056bf7d16529
pancursolucoes <pancursolucoes@gmail.com>
2026-08-23 19:13:26 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
-import NotFound from "./pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
-import Fornecedores from "./pages/Fornecedores";
+
+function NotFound() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>404 - Página Não Encontrada</h1>
+      <a href="/">Voltar para o Início</a>
+    </div>
+  );
+}
 
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/fornecedores"} component={Fornecedores} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
