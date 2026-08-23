dc1e60b0185b692edb3818675e50ffa36c73eafd
pancursolucoes <pancursolucoes@gmail.com>
2026-08-23 18:20:09 -0300

    Create vite.config.ts

---
+++ client/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "dist/public"),
+    emptyOutDir: true,
+  },
+});
