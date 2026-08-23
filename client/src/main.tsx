763a6f6a87f6372886291d6c2d7c8386656d02e9
pancursolucoes <pancursolucoes@gmail.com>
2026-08-23 19:01:09 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,8 @@
+TypeScript
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "../index.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
